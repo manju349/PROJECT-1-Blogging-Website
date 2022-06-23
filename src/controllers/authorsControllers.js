1a336d4164a34ca883f8b603b6255af2973d847c
Subhajit Bokshi <subhajitb17@gmail.com>
2022-06-23 15:46:23 +0530

    Initial

--- src/controllers/authorsControllers.js
+++ src/controllers/authorsControllers.js
@@ -1,4 +1,5 @@
 const authorModel = require('../models/authorsModel')
+const jwt = require("jsonwebtoken")
 
 const createAuthor= async function(req,res){
     try{
@@ -14,3 +15,30 @@ const createAuthor= async function(req,res){
     }
 }
 module.exports.createAuthor= createAuthor
+
+
+const authorLogin = async function(req, res){
+    try{
+        let authorName = req.body.emailId;
+        let password = req.body.password;
+
+        let user = await authorModel.findOne({email: authorName, password: password});
+        if(!user){
+            return res.status(404).send({status: false, msg:"User Name or the Password is not correct"});
+        }
+
+        let token = jwt.sign(
+            {
+                authorId: author._id.toString()
+            },
+            "room-9"
+        );
+        res.setHeader("x-api-key", token);
+        return res.status(201).send({status: ture, token: token })
+    }
+    catch (error){
+        console.log("Server Error:", error.message)
+        res.status(500).send({ msg: "Server Error", error: error.message })
+    }
+}
+module.exports.authorLogin = authorLogin
